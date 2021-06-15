5344e1e048aa3c61c939608271c2d41082760dec
kerstinLux <82453755+kerstinLux@users.noreply.github.com>
2021-06-15 12:25:09 +0200

    Delete Fig3 directory

--- Fig3/calculate_HankelDetmod.m
+++
@@ -1,9 +0,0 @@
-function [d] = calculate_HankelDetmod(m_Beta,i,k)
-% Calculates the modified Hankel determinant as in "DistApproxandModellingViaOrthogonalPolynomialSequences", Provost (2015)
-M = zeros(i,i);
-for r=0:i-1
-    M(r+1,:) = [m_Beta(r+1:k-1+r+1)' m_Beta(k+1+r+1:i+r+1)'];
-end
-d = det(M);
-end
-
